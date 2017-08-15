85c55f45fa2b485a27f73d3c41407f428646874b
wuhaibo <haibowu@hotmail.com>
2017-08-15 21:23:43 +0800

    add scatter animation

--- src/main/webapp/echarts/ddos.js
+++ src/main/webapp/echarts/ddos.js
@@ -1,3 +1,66 @@
+var initialOption = {
+	geo: {
+		zoom: 1,
+		center: null
+	}
+};
+
+var targetDefaultScatterStyle = {
+  symbolSize: 6,
+  itemStyle: {
+        normal: {
+            color: 'orange',
+            borderColor: '#000'
+        }
+  }
+}
+
+var targetViolentScatterStyle = {
+  symbolSize: 8,
+  itemStyle: {
+        normal: {
+            color: 'red',
+            borderColor: '#000'
+        }
+  }
+}
+
+var attackerDefaultScatterStyle = {
+  symbolSize: 4,
+  itemStyle: {
+        normal: {
+        	color: '#0D6695',
+            borderColor: 'gold'
+        }
+  }
+}
+
+var attackerActiveScatterStyle = {
+  symbolSize: 6,
+  itemStyle: {
+        normal: {
+        	color: '#a6c84c',
+            borderColor: 'gold'
+        }
+  }
+}
+
+var pageStatus = {
+  activeAttackArea: '',
+  attackAreas: [],
+  attackViolent: false
+};
+//var activeAttackArea;
+//var attackAreas = [];
+
+function getTargetScatterStyle() {
+	return (pageStatus.attackViolent) ? targetViolentScatterStyle : targetDefaultScatterStyle;
+}
+
+function getAttackerScatterStyle(attackArea) {
+	return (pageStatus.attackAreas.indexOf(attackArea)>=0) ? attackerActiveScatterStyle : attackerDefaultScatterStyle;
+}
+
 var geoCoordMap = {
     '天津': [117.4219, 39.4189],
     '河北': [114.4995,38.1006],
@@ -12,39 +75,13 @@ var sourceArea = '北京';
 
 function buildAttackLines() {
   var planeLines = [];
-  for (var i = 0; i < attackAreas.length; i++) {
+  for (var i = 0; i < pageStatus.attackAreas.length; i++) {
     planeLines.push({ 
-      coords: [geoCoordMap[attackAreas[i]], geoCoordMap[sourceArea]] 
+      coords: [geoCoordMap[pageStatus.attackAreas[i]], geoCoordMap[sourceArea]] 
     });
   }
   return planeLines;
 }
-
-function buildAreaScatter() {
-  var areaScatters = [];
-  areaScatters.push({
-      name: sourceArea,
-      value: geoCoordMap[sourceArea],
-      symbolSize: 4,
-      itemStyle: {
-          normal: {
-              color: 'red',
-              borderColor: '#000'
-          }
-      }
-  });
-    
-  for (var i = 0; i < attackAreaBases.length; i++) {
-    areaScatters.push({
-        name: attackAreaBases[i],
-        value: geoCoordMap[attackAreaBases[i]]
-    });
-  }
-  return areaScatters;
-}
-
-
-
 
 //var planePath = 'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z';
 var planePath = 'arrow';
@@ -54,7 +91,7 @@ var planeSerie = {
     zlevel: 2,
     effect: {
         show: true,
-        period: 6,
+        period: 1,
         trailLength: 0.1,
         color: 'maroon',
         symbol: planePath,
@@ -69,6 +106,28 @@ var planeSerie = {
         }
     },
     data: buildAttackLines()
+}
+
+
+function buildAreaScatter() {
+  var areaScatters = [];
+
+  var scatter = {
+		name: sourceArea,
+		value: geoCoordMap[sourceArea],
+  };
+  $.extend(scatter, getTargetScatterStyle());
+  areaScatters.push(scatter);
+    
+  for (var i = 0; i < attackAreaBases.length; i++) {
+	var areaScatter = {
+        name: attackAreaBases[i],
+        value: geoCoordMap[attackAreaBases[i]]
+    };
+	$.extend(areaScatter, getAttackerScatterStyle(attackAreaBases[i]));
+    areaScatters.push(areaScatter);
+  }
+  return areaScatters;
 }
 
 var scatterSerie = {
@@ -87,16 +146,11 @@ var scatterSerie = {
             formatter: '{b}'
         }
     },
-    symbolSize: 4,
-    itemStyle: {
-        normal: {
-            color: '#0D6695',
-            borderColor: 'gold'
-        }
-    },
 
     data: buildAreaScatter()
 } 
+
+
 
 var series = [ planeSerie, scatterSerie ];
 
