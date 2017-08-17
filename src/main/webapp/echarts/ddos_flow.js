2f97db7b8e3fb02f67c94e6e4f062dc63c9b1341
wuhaibo <haibowu@hotmail.com>
2017-08-17 21:35:51 +0800

    adjust beijing flow charts style

--- src/main/webapp/echarts/ddos_flow.js
+++ src/main/webapp/echarts/ddos_flow.js
@@ -1,79 +1,92 @@
+function paddingTime(value) {
+  return value < 10 ? '0'+value : value;
+}
 function randomData() {
-    now = new Date(+now + oneDay);
-    value = value + Math.random() * 21 - 10;
-    return {
-        name: now.toString(),
-        value: [
-            [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
-            Math.round(value)
-        ]
-    }
+  now = new Date(+now + interval);
+  value = value + Math.random() * 21 - 10;
+  return {
+    value : [ now, Math.round(value) ]
+  }
 }
 
-var data = [];
-var now = +new Date(1997, 9, 3);
-var oneDay = 24 * 3600 * 1000;
+var timeValue = [];
+var now = +new Date();
+var interval = 200;
 var value = Math.random() * 1000;
 for (var i = 0; i < 1000; i++) {
-    data.push(randomData());
+  timeValue.push(randomData());
 }
 
 beijingFlowOption = {
-    title: {
-    	left: 'center',
-        text: '北京流量',
-        textStyle : {
-            color: '#e0e0e0'
-        }
+  title : {
+    left : 'center',
+    text : '北京流量',
+    textStyle : {
+      color : '#e0e0e0'
+    }
+  },
+  grid : {
+    left : 40,
+    right : 20
+  },
+  tooltip : {
+    trigger : 'axis',
+    formatter : function(params) {
+      params = params[0];
+      var date = new Date(params.name);
+      return params.value[1];
     },
-    tooltip: {
-        trigger: 'axis',
-        formatter: function (params) {
-            params = params[0];
-            var date = new Date(params.name);
-            return params.value[1];
-        },
-        axisPointer: {
-            animation: false
-        }
+    axisPointer : {
+      animation : false
+    }
+  },
+  xAxis : {
+    type : 'time',
+    axisLabel : {
+      formatter : (function(value) {
+        let t = new Date(value);
+        return paddingTime(t.getMinutes()) + ":" + paddingTime(t.getSeconds());
+      })
     },
-    xAxis: {
-        type: 'time',
-        splitLine: {
-            show: false
-        }
+    splitLine : {
+      show : false
     },
-    yAxis: {
-        type: 'value',
-        boundaryGap: [0, '100%'],
-        splitLine: {
-            show: true
-        }
+    min: 'dataMin'
+  },
+  yAxis : {
+    type : 'value',
+    boundaryGap : [ 0, '100%' ],
+    splitLine : {
+      show : true
+    }
+  },
+  series : [ {
+    name : '模拟数据',
+    type : 'line',
+    showSymbol : false,
+    hoverAnimation : false,
+    areaStyle : {
+      normal : {
+        color : '#00FA9A'
+      }
     },
-    series: [{
-        name: '模拟数据',
-        type: 'line',
-        showSymbol: false,
-        hoverAnimation: false,
-        areaStyle: {
-            normal: {
-                color: '#d2d2d2'
-            }
-        },
-        data: data
-    }]
+    data : timeValue
+  } ]
 };
 
-setInterval(function () {
+setInterval(function() {
 
-    for (var i = 0; i < 5; i++) {
-        data.shift();
-        data.push(randomData());
-    }
+  for (var i = 0; i < 5; i++) {
+    timeValue.shift();
+    timeValue.push(randomData());
+  }
 
-    beijingFlowChart.setOption({
-        series: [{
-            data: data
-        }]
-    });
+  beijingFlowChart.setOption({
+    xAxis : {
+
+    },
+    series : [ {
+      data : timeValue
+    } ]
+  });
 }, 1000);
