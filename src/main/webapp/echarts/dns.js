72f55c0ed621a6f868eed347f60223f3567562e0
Wu Haibo <haibowu@hotmail.com>
2017-08-31 15:02:55 +0800

    add defense effect on dns map

--- src/main/webapp/echarts/dns.js
+++ src/main/webapp/echarts/dns.js
@@ -68,6 +68,24 @@ function buildAttackLines() {
   return planeLines;
 }
 
+function buildDefensingLines() {
+  if(WebPage.pageStatus.status != 'defense')
+    return [];
+  
+  var planeLines = [];
+  planeLines.push({ 
+    coords: [geoCoordMap[WebPage.pageStatus.attackArea], geoCoordMap[WebPage.pageStatus.targetArea]] 
+  });
+  return planeLines;
+}
+
+function buildDefenseMarkPoint() {
+  if(WebPage.pageStatus.status != 'defense') 
+    return [];
+
+  return [{ name: '协同', coord: geoCoordMap[WebPage.pageStatus.attackArea] }];
+}
+
 //var planePath = 'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z';
 var planePath = 'arrow';
 
@@ -134,6 +152,20 @@ function buildAreaScatter() {
   return areaScatters;
 }
 
+var defensingTrailSerie = $.extend(true, {}, trailSerie, {
+  effect: {
+    period: 10,
+    trailLength: 0.7,
+  },
+  data: buildDefensingLines()
+});
+
+var defensingPlaneSerie = $.extend(true, {}, planeSerie, {
+  effect: {
+    period: 10,
+  },
+  data: buildDefensingLines()
+});
 
 var scatterSerie = {
     type: 'effectScatter',
@@ -151,12 +183,31 @@ var scatterSerie = {
             formatter: '{b}'
         }
     },
+    markPoint: {
+      symbol: 'circle',
+      symbolSize: 70,
+      itemStyle: {
+        normal: {
+          color: 'rgba(128, 128, 255, 0)',
+          borderColor: '#1e90ff',
+          borderWidth: 5
+        }
+      },
+      label:{
+          normal:{
+              show: true,
+              position: 'insideTop',
+              formatter:function(d){return d.name}
+          }
+      },
+      data: buildDefenseMarkPoint()
+    },
     
     data: buildAreaScatter()
 }
 
 
-var series = [ trailSerie, planeSerie, scatterSerie];
+var series = [ trailSerie, planeSerie, scatterSerie, defensingTrailSerie, defensingPlaneSerie];
 
 var initialGeo = {
     map: 'china',
