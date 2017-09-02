5064f5f866a8ff96a471d571fe643324b4324e40
wuhaibo <haibowu@hotmail.com>
2017-09-02 21:59:03 +0800

    complete webpage dashboard 6 status display

--- src/main/java/com/bochum/nade/api/WebPageServlet.java
+++ src/main/java/com/bochum/nade/api/WebPageServlet.java
@@ -26,6 +26,7 @@ public class WebPageServlet extends JsonResponseServlet {
 		
 		if ("attack".equals(request.getServletContext().getAttribute("action"))) {
 			map.put("status", "attack");
+			map.put("alarm_title", "演练启动");
 			map.put("alarm_text", "攻击者发起口令破解攻击。");
 		} else if ("alarm".equals(request.getServletContext().getAttribute("action"))) {
 			map.put("status", "alarm");
@@ -34,16 +35,20 @@ public class WebPageServlet extends JsonResponseServlet {
 			map.put("screenshot", "distort_webpage.jpg");
 		} else if ("analyze".equals(request.getServletContext().getAttribute("action"))) {
 			map.put("status", "analyze");
+			map.put("defense_title", "事件研判");
 			map.put("defense_text", "攻击源IP地址：125.39.240.113");
 		} else if ("repair".equals(request.getServletContext().getAttribute("action"))) {
 			map.put("status", "repair");
+			map.put("defense_title", "应急处置");
 			String[] displayTexts={"更新应用服务器管理员口令","恢复被篡改的页面代码","升级应用服务器系统补丁", "查杀系统病毒"};
 			map.put("defense_text", displayTexts);
 		} else if ("defense".equals(request.getServletContext().getAttribute("action"))) {
 			map.put("status", "defense");
+			map.put("defense_title", "协同处理");
 			map.put("defense_text", "封堵攻击源IP地址：125.39.240.113");
 		} else if ("confirm".equals(request.getServletContext().getAttribute("action"))) {
 			map.put("status", "confirm");
+			map.put("defense_title", "结果验证");
 			map.put("defense_text", "验证官网主页恢复");
 			map.put("screenshot", "tianjin_dianxin.jpg");
 		} else {
